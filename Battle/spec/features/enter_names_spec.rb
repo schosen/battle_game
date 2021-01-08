a8df04bc45977414e513fe5ad91c63b34e76cee4
Sarah <sarahchosen@hotmail.co.uk>
2021-01-08 14:18:46 +0000

    refactored with helper method

--- Battle/spec/features/enter_names_spec.rb
+++ Battle/spec/features/enter_names_spec.rb
@@ -5,10 +5,7 @@ Capybara.app = Battle
 
 feature 'Testing name form' do 
   scenario 'can fill in name on form, and see name on-screen' do
-    visit('/')
-    fill_in :player_1, with: 'Sarah'
-    fill_in :player_2, with: 'Michael'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Sarah vs. Michael'
   end
 end 
